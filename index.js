98945284e0288b13a300e1d941c845282f24b8b9
montassarabidi <montssarabidi@gmail.com>
2022-08-08 00:28:31 +0100

    algo4

---
+++ index.js
@@ -0,0 +1,10 @@
+function x(v1:Algorithm,v2:Algorithm,ps:Integer) {
+for(i=0,i>v1.length,i++)
+ps=ps+v1[i]+v2[i]
+return ps
+}
+ps.length=0
+ps=x(v1,v2,ps) or false
+if(ps=0)
+x or true
+else(v2 or ps)
